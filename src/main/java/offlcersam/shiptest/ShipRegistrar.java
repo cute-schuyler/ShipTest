18277b6213975dbab75c4cf3f058c0b2c0bef61b
cute-schuyler <schuylerjm123@gmail.com>
2026-07-19 16:19:27 -0400

    Update ShipRegistrar.java

--- src/main/java/offlcersam/shiptest/ShipRegistrar.java
+++ src/main/java/offlcersam/shiptest/ShipRegistrar.java
@@ -56,71 +56,222 @@ public final class ShipRegistrar {
         float integ = 200.0F;
         float carg = 75.0F * cargoMod;
 
-        ShipList.write(
-                registerShipID(350),
-                30,                        // Int: Icon, sets Icon according to sprite sheet.
-                Color.AZURE,                    // Color: Color, unsure what exactly this affects.
-                "Arrowhead",                    // String: Display name
-                "Maybe one day you could be a real arrow.", // String: Display description
-                0,                              // Int: Tier, affects spawning and what level it's usable at.
-                TypeTag.UNCOMMON,               // TypeTag, Affects spawning and loot drop, I think.
-                350,                            // Int: Render Index, the ship's sprite
-                37,                             // Int: Engine Position glow in pixels
-                integ * 1.50F,                  // Float: Hull HP (integ * multiplier), somewhat based off ShipList style of doing it.
-                carg * 1.10F,                   // Float: Cargo (carg * multiplier), also based off ShipList style of doing it.
-                WeaponSlotLayoutList.S_2_V,     // WeaponSlotLayoutList: Weapon Layout, see WeaponSlotLayoutList for full list.
-                2,                              // Int: Energy slots, unsure what the UI limit for slots are but base game doesn't go above 8 currently.
-                1,                              // Int: Armor slots
-                1,                              // Int: Shield slots
-                0,                              // Int: Device slots
-                1,                              // Int: Module slots
-                1                               // Int: Engine slots
-        );
+        //ShipList.write(
+        //        registerShipID(350),
+        //        30,                         Int: Icon, sets Icon according to sprite sheet.
+        //        Color.AZURE,                     Color: Color, unsure what exactly this affects.
+        //        "Arrowhead",                     String: Display name
+        //        "Maybe one day you could be a real arrow.",  String: Display description
+        //        0,                               Int: Tier, affects spawning and what level it's usable at.
+        //        TypeTag.UNCOMMON,               TypeTag, Affects spawning and loot drop, I think.
+        //        350,                             Int: Render Index, the ship's sprite
+        //        37,                              Int: Engine Position glow in pixels
+        //        integ * 1.50F,                   Float: Hull HP (integ * multiplier), somewhat based off ShipList style of doing it.
+        //        carg * 1.10F,                    Float: Cargo (carg * multiplier), also based off ShipList style of doing it.
+        //        WeaponSlotLayoutList.S_2_V,      WeaponSlotLayoutList: Weapon Layout, see WeaponSlotLayoutList for full list.
+        //        2,                               Int: Energy slots, unsure what the UI limit for slots are but base game doesn't go above 8 currently.
+        //        1,                               Int: Armor slots
+        //        1,                               Int: Shield slots
+        //        0,                              Int: Device slots
+        //      1,                              Int: Module slots
+        //       1                                Int: Engine slots
+        //);
 
         float integ2 = 200;
         float carg2 = 350.0F * cargoMod * 2.0F;
 
         ShipList.write(
-                registerShipID(40),
+                registerShipID(250),
                 158,
                 Color.WHITE,
-                "Foundry",
-                "Build an even bigger megastructure.",
-                4,
-                TypeTag.RARE,
-                349,
-                64,
-                integ2 * 1.20F,
-                carg2 * 1.3F,
-                WeaponSlotLayoutList.S_6_V,
-                6,
-                5,
-                4,
+                "Caterpillar",
+                "Heavily Armored Transport.",
                 2,
-                5,
-                4
+                TypeTag.UNCOMMON,
+                850,
+                190,
+                integ2 * 1.50F,
+                carg2 * 1.5F,
+                WeaponSlotLayoutList.S_1_V,
+                4,
+                6,
+                2,
+                1,
+                3,
+                2
         );
 
         ShipList.write(
-                registerShipID(41),
-                216,
-                Color.PURPLE,
-                "Foundry+",
-                "Build an even bigger megastructure+.",
-                5,
-                TypeTag.EXOTIC,
-                349,
-                64,
-                integ2 * 1.5F,
-                carg2 * 1.5F,
-                WeaponSlotLayoutList.S_7_V,
+                registerShipID(251),
+                158,
+                Color.WHITE,
+                "Container",
+                "For your long range transport needs.",
+                2,
+                TypeTag.UNCOMMON,
+                851,
+                218,
+                integ2 * 1.1F,
+                carg2 * 1.8F,
+                WeaponSlotLayoutList.S_2_V,
                 6,
-                6,
-                5,
+                2,
+                2,
+                1,
                 3,
-                6,
                 5
         );
+
+                ShipList.write(
+                registerShipID(252),
+                158,
+                Color.WHITE,
+                "Carrier HE",
+                "Heavily Armored Transport+.",
+                4,
+                TypeTag.RARE,
+                852,
+                480,
+                integ2 * 1.8F,
+                carg2 * 1.8F,
+                WeaponSlotLayoutList.S_5_V,
+                6,
+                8,
+                3,
+                2,
+                3,
+                4
+        );
+
+                ShipList.write(
+                registerShipID(253),
+                216,
+                Color.WHITE,
+                "Delameya",
+                "Assault Gunship.",
+                4,
+                TypeTag.RARE,
+                853,
+                340,
+                integ2 * 1.8F,
+                carg2 * 1.8F,
+                WeaponSlotLayoutList.S_6_V,
+                5,
+                4,
+                4,
+                4,
+                4,
+                2
+        );
+
+                ShipList.write(
+                registerShipID(254),
+                30,
+                Color.WHITE,
+                "Gemini",
+                "Twin Destroyer.",
+                1,
+                TypeTag.COMMON,
+                854,
+                54,
+                integ * 1.6F,
+                carg * 1.1F,
+                WeaponSlotLayoutList.S_4_V,
+                3,
+                2,
+                2,
+                2,
+                2,
+                2
+        );
+
+                ShipList.write(
+                registerShipID(255),
+                216,
+                Color.WHITE,
+                "Barrier",
+                "A rarely seen Minelayer.",
+                5,
+                TypeTag.EXOTIC,
+                855,
+                310,
+                integ2 * 2.0F,
+                carg2 * 3.0F,
+                WeaponSlotLayoutList.S_4_V,
+                7,
+                3,
+                3,
+                6,
+                6,
+                4
+        );
+
+                ShipList.write(
+                registerShipID(256),
+                30,
+                Color.WHITE,
+                "Xport",
+                "Light Military Hauler.",
+                1,
+                TypeTag.COMMON,
+                856,
+                340,
+                integ * 1.2F,
+                carg * 1.6F,
+                WeaponSlotLayoutList.S_3_V,
+                2,
+                2,
+                2,
+                0,
+                2,
+                2
+        );
+
+                ShipList.write(
+                registerShipID(257),
+                158,
+                Color.WHITE,
+                "Dominator",
+                "Common Empire Destroyer.",
+                1,
+                TypeTag.COMMON,
+                857,
+                424,
+                integ2 * 1.5F,
+                carg2 * 1.5F,
+                WeaponSlotLayoutList.S_3_V,
+                3,
+                2,
+                2,
+                1,
+                1,
+                3
+        );
+
+                ShipList.write(
+                registerShipID(258),
+                158,
+                Color.WHITE,
+                "Exterminator",
+                "Heavy Empire Destroyer.",
+                2,
+                TypeTag.UNCOMMON,
+                858,
+                395,
+                integ2 * 2.0F,
+                carg2 * 1.6F,
+                WeaponSlotLayoutList.S_5_V,
+                4,
+                3,
+                3,
+                2,
+                2,
+                4
+        );
+
+
+
+        
 
 
         ShipList.loadShipStatsFromItems(_database.ItemDatabase.itemDataFile);
