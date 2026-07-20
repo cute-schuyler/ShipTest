eee670b8589b0a4cec8c91f4f3191c0c1c13432d
cute-schuyler <schuylerjm123@gmail.com>
2026-07-20 04:29:41 -0400

    Update CraftingRegistrarMixin.java

--- src/main/java/offlcersam/shiptest/mixin/CraftingRegistrarMixin.java
+++ src/main/java/offlcersam/shiptest/mixin/CraftingRegistrarMixin.java
@@ -1,4 +1,4 @@
-package offlcersam.shiptest.mixin;
+package garmillas.mixin;
 
 import crafting.CraftingTable;
 import crafting.CraftingTableNormal;
@@ -18,11 +18,18 @@ public abstract class CraftingRegistrarMixin extends CraftingTable {
     private void shiptest$registerShipRecipe(CallbackInfo ci) {
 
         // Readability, ShipIDs are essentially BaseID + 10000
-        int ARROWHEAD_ID = 100350;
-        int FOUNDRY_ITEM_ID = 100040;
-        int FOUNDRY_PLUS_ITEM_ID = 100041;
+        int CATERPILLAR_GE = 100550;
+        int CONTAINER_GE = 100551;
+        int CARRIERHE_GE = 100552;
+        int DELAMEYA_GE = 100553;
+        int GEMINI_GE = 100554;
+        int BARRIER_GE = 100555;
+        int XPORT_GE = 100556;
+        int DOMINATOR_GE = 100557;
+        int EXTERMINATOR_GE = 100558;
 
         // Uses market IDs of ships (100000 + ID), these are here for readability.
+        int skiffItemId = 100000;
         int yachtItemId = 100001;
         int fabricatorItemId = 100029;
         int fabricatorplusItemId = 100039;
@@ -36,50 +43,114 @@ public abstract class CraftingRegistrarMixin extends CraftingTable {
         int FeMatx = 10711, AlMatx = 10712, TiMatx = 10713, WgMatx = 10714, VaMatx = 10715;
 
         this.addRecipe(
-                "T1:Arrowhead", // String: Label, no specific naming convention, copied what it looked like in CraftingTableNormal.
-                ARROWHEAD_ID, // Int: Market ID of Ship
-                bpShipT0_1, 1, // Int: Blueprint ID, Int: Amount to use
-                yachtItemId, 1, // Int: Item A, Int: Amount to use
-                AgNode, 6, // Int: Item B, Int: Amount to use
-                FeMatx, 8 // Int: Item C, Int: Amount to use
+                "T1:Gemini",
+                GEMINI_GE,
+                bpShipT0_1, 1,
+                skiffItemId, 1,
+                CuNode, 10,
+                FeMatx, 10
         );
 
         this.addRecipe(
-                "T4:Foundry",
-                FOUNDRY_ITEM_ID,
+                "T0:Xport",
+                XPORT_GE,
+                bpShipT0_1, 1,
+                skiffItemId, 1,
+                CuNode, 5,
+                FeMatx, 5
+        );
+
+        this.addRecipe(
+                "T1:Dominator",
+                DOMINATOR_GE,
+                bpShipT0_1, 1,
+                skiffItemId, 1,
+                CuNode, 10,
+                FeMatx, 10
+        );
+
+        this.addRecipe(
+                "T1:Dominator+Alt1",
+                DOMINATOR_GE,
+                bpShipT0_1, 1,
+                XPORT_GE, 1,
+                CuNode, 5,
+                FeMatx, 5
+        );
+
+        this.addRecipe(
+                "T2:Exterminator",
+                EXTERMINATOR_GE,
+                bpShipT2, 1,
+                DOMINATOR_GE, 1,
+                AgNode, 20,
+                AlMatx, 20
+        );
+
+        this.addRecipe(
+                "T2:Exterminator+Alt1",
+                EXTERMINATOR_GE,
+                bpShipT2, 1,
+                GEMINI_GE, 2,
+                AgNode, 10,
+                AlMatx, 10
+        );
+
+        this.addRecipe(
+                "T2:Caterpillar",
+                CATERPILLAR_GE,
+                bpShipT2, 1,
+                XPORT_GE, 1,
+                AgNode, 20,
+                AlMatx, 20
+        );
+
+        this.addRecipe(
+                "T2:Container",
+                CONTAINER_GE,
+                bpShipT2, 1,
+                XPORT_GE, 2,
+                AgNode, 20,
+                AlMatx, 20
+        );
+
+        this.addRecipe(
+                "T4:Carrier HE",
+                CARRIERHE_GE,
                 bpShipT4_5, 1,
-                fabricatorItemId, 2,
+                CATERPILLAR_GE, 1,
+                CONTAINER_GE, 1,
                 AuNode, 20,
+                TiMatx, 20
+        );
+
+        this.addRecipe(
+                "T4:Delameya",
+                DELAMEYA_GE,
+                bpShipT4_5, 1,
+                EXTERMINATOR_GE, 1,
+                AuNode, 20,
+                TiMatx, 20
+        );
+
+        this.addRecipe(
+                "T5:Barrier",
+                BARRIER_GE,
+                bpShipT4_5, 1,
+                EXTERMINATOR_GE, 1,
+                CATERPILLAR_GE, 1,
+                AuNode, 40,
                 TiMatx, 40
         );
 
-        this.addRecipe(
-                "T4:Foundry+Alt1",
-                FOUNDRY_ITEM_ID,
-                bpShipT4_5, 1,
-                fabricatorplusItemId, 1,
-                AuNode, 10,
-                TiMatx, 25
-        );
 
-        this.addRecipe(
-                "T5:FoundryPlus",
-                FOUNDRY_PLUS_ITEM_ID,
-                bpShipT4_5, 1,
-                FOUNDRY_ITEM_ID, 1,
-                PtNode, 10,
-                WgMatx, 30
-        );
 
-        this.addRecipe(
-                "T5:FoundryPlus+Alt1",
-                FOUNDRY_PLUS_ITEM_ID,
-                bpShipT4_5, 1,
-                fabricatorplusItemId, 2,
-                PtNode, 15,
-                WgMatx, 50
-        );
 
-        ModLogger.log("[ShipTest] Ship recipes added.");
+
+
+
+
+
+        ModLogger.log("[Gam] Ship recipes added.");
     }
 }
